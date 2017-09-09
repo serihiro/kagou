e63e4db54b509f1b94c83ec1ecf4d53d5cdcc36a
Kazuhiro Serizawa <nserihiro@gmail.com>
2017-09-09 17:03:44 +0900

    malloc request_header_values explicitly

--- src/request.h
+++ src/request.h
@@ -10,6 +10,7 @@ typedef struct {
 } Request;
 
 #define REQUEST_HEADER_VALUE_SIZE 10
+#define RESPONSE_HEADER_VALUE_BUFFER_SIZE 1025 // 1KiB + 1byte
 #define ATTRIBUTE_DELIMITER " "
 
 Request *Request_new(char *raw_request);
